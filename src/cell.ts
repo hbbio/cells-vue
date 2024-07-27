d040dcf044435fbace0572a7bdef0ea5b5eba0d0
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-07-27 19:04:47 +0800

    cell: log errors

--- src/cell.ts
+++ src/cell.ts
@@ -4,11 +4,13 @@ import type { AnyCell } from "@okcontract/cells";
 
 export const useCell = <T>(cell: AnyCell<T>) => {
 	const state = ref(undefined as undefined | T);
-	const unsubscribe = cell.subscribe((value) => {
-		if (!(value instanceof Error)) state.value = value as UnwrapRef<T>;
-	});
-	onUnmounted(() => {
-		unsubscribe();
-	});
+	onUnmounted(
+		cell.subscribe((value) => {
+			if (value instanceof Error)
+				// @todo integrate error management with Vue?
+				console.log(`error in cell[${cell.id}]: ${value}`);
+			else state.value = value as UnwrapRef<T>;
+		}),
+	);
 	return state;
 };
