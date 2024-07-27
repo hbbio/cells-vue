76a0161d94aa1f6e84c735b85abdeeae1084e9eb
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2024-07-27 18:58:05 +0800

    app: sample count cell

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+
+import { Sheet } from "@okcontract/cells"
+
+import { useCell } from "./cell"
+
+const sheet = new Sheet()
+const count = sheet.new(0)
+const countState = useCell(count)
 </script>
 
 <template>
@@ -8,16 +15,17 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <p>Count: {{ countState }}</p>
+      <button @click="() => count.update(v => v + 1)">Incr</button>
 
-      <nav>
+      <!--       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </nav> -->
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
